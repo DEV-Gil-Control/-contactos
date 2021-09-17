3ed1e20551bf3a6a7d662e15161a900699d65a57
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-17 08:16:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function getdata(){
-  	var user=document.getElementById("user").value;
-	firebase.database().ref(user).once('value').then(function (snapshot){
+  	var user=document.getElementById("messages").value;
+	firebase.database().ref(messages).once('value').then(function (snapshot){
   		snapshot.forEach(function(childSnapshot)
   			{
   				//var childKey=user;
   				//var childData=childSnapshot.val();
-  				document.getElementById("namemenor").innerHTML = snapshot.val().Child;
+  				document.getElementById("namemenor").innerHTML = snapshot.val().namemenor;
 
 
   		})
