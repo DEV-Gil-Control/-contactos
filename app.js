1e289a7575ed733d7955ad626bf828bfe18c459f
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-17 08:12:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ function getdata(){
   			{
   				//var childKey=user;
   				//var childData=childSnapshot.val();
-  				document.getElementById("name").innerHTML = snapshot.val().Child;
-  				document.getElementById("age").innerHTML = snapshot.val().age;
-  				document.getElementById("Phno1").innerHTML = snapshot.val().Phno1;
-  				document.getElementById("Phno2").innerHTML = snapshot.val().Phno2;
-  				document.getElementById("blood").innerHTML = snapshot.val().Bloodgroup;
-  				document.getElementById("address").innerHTML = snapshot.val().Address;
-  				document.getElementById("allergy").innerHTML = snapshot.val().allergy;
+  				document.getElementById("namemenor").innerHTML = snapshot.val().Child;
+
 
   		})
   			
